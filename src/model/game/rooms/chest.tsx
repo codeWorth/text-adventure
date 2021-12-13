7e9d65408385d692e074e215f396fefcd1aa7ab8
Andrew Cummings <andrewc04@g.ucla.edu>
2021-12-13 04:23:15 -0800

    extract to chest class

--- src/model/game/rooms/chest.tsx
+++ src/model/game/rooms/chest.tsx
@@ -1,15 +1,62 @@
+import LogAction from "../../userinput/actions/logAction";
+import PureAction from "../../userinput/actions/pureAction";
+import Option from "../../userinput/option";
 import Game from "../game";
+import Player from "../player";
 import TakeableItems, { ItemInfo } from "./takeableItems";
+
+class ChestBuilder {
+    public _openMessage: string = "You unlatch the chest and open it up.";
+    public _lookMessage: string = "The chest is held closed with a simple latch, which shouldn't be too tough to open.";
+    public _names: string[] = [];
+    public _items: ItemInfo[];
+
+    private constructor(items: ItemInfo[]) {
+        this._items = items;
+    }
+
+    openMessage(message: string): ChestBuilder {
+        this._openMessage = message;
+        return this;
+    }
+
+    lookMessage(message: string): ChestBuilder {
+        this._lookMessage = message;
+        return this;
+    }
+
+    names(...names: string[]): ChestBuilder {
+        this._names = names;
+        return this;
+    }
+
+    build() {
+        return new Chest(
+            this._items, 
+            this._openMessage, 
+            this._lookMessage, 
+            this._names.map(name => " " + name)
+        );
+    }
+
+    public static withItems(...items: ItemInfo[]) {
+        return new ChestBuilder(items);
+    }
+}
 
 class Chest {
 
     private readonly takeableItems: TakeableItems;
     private readonly openMessage: string;
+    private readonly lookMessage: string;
+    private readonly names: string[];
     private _isOpen: boolean;
 
-    constructor(items: ItemInfo[], openMessage: string | undefined) {
+    constructor(items: ItemInfo[], openMessage: string, lookMessage: string, names: string[]) {
         this.takeableItems = new TakeableItems(...items);
-        this.openMessage = openMessage || "You unlatch the chest and open it up.";
+        this.openMessage = openMessage;
+        this.lookMessage = lookMessage;
+        this.names = names;
         this._isOpen = false;
     }
 
@@ -28,6 +75,29 @@ class Chest {
         this._isOpen = true;
     }
 
+    getLookOptions(): Option[] {
+        return [
+            ...Option.forNames(
+                !this._isOpen 
+                    ? new LogAction(this.lookMessage)
+                    : new LogAction("You've already opened this chest."),
+                ...this.names
+            ),
+            ...this.takeableItems.getLookAtOptions()
+        ];
+    }
+
+    getOpenOptions(): Option[] {
+        if (this._isOpen) return [];
+        return Option.forNames(
+            new PureAction(game => this.open(game)),
+            ...this.names
+        );
+    }
+
+    getTakeOptions(player: Player): Option[] {
+        return this.takeableItems.getTakeOptions(player);
+    }
 }
 
-export default Chest;
+export { Chest, ChestBuilder };
--- src/model/game/rooms/chestRoom.tsx
+++ src/model/game/rooms/chestRoom.tsx
@@ -1,24 +1,24 @@
 import Room from "../room";
-import TakeableItems from "./takeableItems";
 import Key from "../items/key";
 import Game from "../game";
 import Option from "../../userinput/option";
 import LookBuilder from "../../userinput/actions/lookBuilder";
 import OptionsBuilder from "../../userinput/actions/optionsBuilder";
-import LogAction from "../../userinput/actions/logAction";
 import { nonNull } from "../../../util";
-import PureAction from "../../userinput/actions/pureAction";
+import { Chest, ChestBuilder } from "./chest";
 
 class ChestRoom extends Room {
 
-    private readonly takeableItems: TakeableItems = new TakeableItems();
-    private readonly largeIronKey: Key;
-    private chestOpen: boolean;
+    private readonly chest: Chest;
 
     constructor(name: string, largeIronKey: Key) {
         super(name);
-        this.largeIronKey = largeIronKey;
-        this.chestOpen = false;
+        this.chest = ChestBuilder.withItems({
+            item: largeIronKey,
+            lookMessage: "Surely this large iron key goes to a large iron door..."
+        })
+            .names("chest", "wooden chest")
+            .build();
     }
 
     getOptions(game: Game): Option[] {
@@ -26,37 +26,19 @@ class ChestRoom extends Room {
             ...super.getOptions(game),
             Option.forAction("take", new OptionsBuilder(
                 "You must specify which item to take.",
-                ...this.takeableItems.getTakeOptions(game.player)
+                ...this.chest.getTakeOptions(game.player)
             )),
             Option.forAction("look", new LookBuilder(
                 "The room is empty except for a wooden chest.\n" + this.connections.getDescription(),
                 new OptionsBuilder(
                     "You must specify what to look at.",
-                    ...Option.forNames(
-                        !this.chestOpen 
-                            ? new LogAction("The chest is held closed with a simple latch, which shouldn't be too tough to open.")
-                            : new LogAction("You've already opened this chest."),
-                        " chest",
-                        " wooden chest"
-                    ),
-                    ...this.takeableItems.getLookAtOptions()
+                    ...this.chest.getLookOptions()
                 )
             )),
-            !this.chestOpen 
-                ? Option.forAction("open", new OptionsBuilder(
-                    "You must specify what to open.",
-                    ...Option.forNames(
-                        new PureAction((game) => {
-                            game.log("You unlatch the chest and open it up. Inside you find:")
-                            game.log(` - ${this.largeIronKey.name}`);
-                            this.takeableItems.addKnownItem(this.largeIronKey, "Surely this large iron key goes to a large iron door...");
-                            this.chestOpen = true;
-                        }),
-                        " chest",
-                        " wooden chest"
-                    )
-                ))
-                : undefined
+            Option.forAction("open", new OptionsBuilder(
+                "You must specify what to open.",
+                ...this.chest.getOpenOptions()
+            ))
         )
     }
 }
