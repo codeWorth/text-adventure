c501b08ab0867328470c46d301ccfb3fa15e4a88
Aidan Cini <aidan@cinidesign.com>
2021-12-19 21:22:58 -0800

    Added the hello option.

--- src/model/game/room.tsx
+++ src/model/game/room.tsx
@@ -1,9 +1,11 @@
 import { nonNull } from '../../util';
 import ActionBuilder from '../userinput/actions/actionBuilder';
 import ActionStart from '../userinput/actions/actionStart';
+import LogAction from '../userinput/actions/logAction';
 import Option from '../userinput/option';
 import Game from './game';
 import Connections from './rooms/connections';
+const helloMsg = `Well ... hello there! Didn't think you'd get a response did you good sir. But alas, I am here. But I can't tell you much ... cause everything is a secret of course. Good day fair traveler! Best of luck to you!`;
 
 class Room {
     public readonly name: string;
@@ -15,7 +17,9 @@ class Room {
     }
 
     getOptions(game: Game): Option[] {
-        return nonNull(this.connections.getGoOption());
+        return nonNull(
+            this.connections.getGoOption(), 
+            Option.forAction("hello", new LogAction(helloMsg)));
     }
 
     getActions(game: Game): ActionBuilder {
